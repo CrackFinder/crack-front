6ea195cc43ac7018c677e82eb1d9365a39525e5d
김규민 <abc2232002@gmail.com>
2025-07-27 14:02:06 +0900

    feat: 포트홀 초기이미지 5개에 대해 잘 동작하게 구현

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 declare global {
   interface Window {
@@ -14,22 +14,42 @@ interface MapProps extends React.HTMLAttributes<HTMLDivElement> {
   longitude: number;
 }
 
+const INITIAL_LEVEL = 5;
+
 function Map({ latitude, longitude, ...rest }: MapProps) {
+  const ref = useRef<HTMLDivElement>(null);
+  const kakaoRef = useRef(window.kakao);
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    const el = ref.current;
+    if (!el) return;
+    const kakao = kakaoRef.current;
+    const options = {
+      center: new kakao.maps.LatLng(latitude, longitude),
+      level: INITIAL_LEVEL,
+    };
+    mapRef.current = new kakao.maps.Map(el, options);
+    const marker = new kakao.maps.Marker({
+      position: new kakao.maps.LatLng(latitude, longitude),
+    });
+
+    marker.setMap(mapRef.current);
+  }, [latitude, longitude]);
+
   return (
-    <div
-      {...rest}
-      style={{ backgroundColor: "red", ...rest.style }}
-      ref={(el) => {
-        if (!el) return;
-        console.log("window.kakao", window.kakao);
-        const kakao = window.kakao;
-        const options = {
-          center: new kakao.maps.LatLng(latitude, longitude),
-          level: 5,
-        };
-        new kakao.maps.Map(el, options);
-      }}
-    />
+    <div style={{ position: "relative", width: "100%", height: "100%" }}>
+      <div {...rest} style={{ width: "100%", height: "100%", ...rest.style }} ref={ref} />
+      <button
+        onClick={() => {
+          mapRef.current?.setCenter(new kakaoRef.current.maps.LatLng(latitude, longitude));
+          mapRef.current?.setLevel(INITIAL_LEVEL);
+        }}
+        style={{ position: "absolute", top: 0, right: 0, zIndex: 1000, backgroundColor: "white" }}
+      >
+        지도 위치 복원
+      </button>
+    </div>
   );
 }
 
