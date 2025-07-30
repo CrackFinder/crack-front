1fca00f684931bd4fd6c605f8127afd1534a5400
김규민 <abc2232002@gmail.com>
2025-07-31 00:05:51 +0900

    feat:쿠키로 변경

--- src/query/meQuery.ts
+++ src/query/meQuery.ts
@@ -1,16 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 import axiosInstance from "./axios";
 import type { UserInfo } from "@/type/User";
-import { accessTokenStore } from "@/store/accessTokenStore";
 
 const getMyUserInfo = async () => {
+  console.log("getMyUserInfo");
   const response = await axiosInstance.get("/user/user");
   return response.data as UserInfo;
 };
 
 export const useMeQuery = (retry: boolean = true) => {
-  const accessToken = accessTokenStore.getState().accessToken;
-  axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
   return useQuery({
     queryKey: ["me"],
     queryFn: getMyUserInfo,
